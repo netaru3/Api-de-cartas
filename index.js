d954a38b50328e4714032788219606c47d5c7365
Aarón <netaru3@gmail.com>
2025-12-10 14:36:22 -0300

    Update index.js
    le agrego el apikey para que solo mi mejor amigo y yo podamos consultar al fetch, (en principio pensaba hacer privado el repositorio y ya, pero entonces nadie podría ver lo que sé)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import {log} from './mongo.js'
-
+import dotenv from 'dotenv'
+dotenv.config()
+let apikey= process.env.API_KEY
 const app= express()
-app.get('/:nombre',async function(req,res){
+app.get(`${apikey}/:nombre`,async function(req,res){
    let carta=await log.find({nombre: req.params.nombre})
 res.json(carta)
 console.log(carta)
