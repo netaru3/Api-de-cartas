b0c2680705010b38c1348c3414b15056aad2f7db
Aarón <netaru3@gmail.com>
2025-12-10 23:25:27 -0300

    Use API key from environment variable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {log} from './mongo.js';
 import dotenv from 'dotenv'
 dotenv.config()
-let apikey= "jejejmenx3"
+let apikey=process.env.API_KEY
 const app= express()
 app.get(`/${apikey}/:nombre`,async function(req,res){
    let carta=await log.find({nombre: req.params.nombre})
