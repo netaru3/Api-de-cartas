39c2ccdc6d57f98bbfd8cc0bcef85937279a5d14
Aarón <netaru3@gmail.com>
2025-12-12 23:36:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ import {log} from './mongo.js';
 import dotenv from 'dotenv'
 import cors from "cors"
 dotenv.config()
+
+
 let apikey=process.env.API_KEY
 const app= express()
+
 app.use(cors())
+
 app.get(`/${apikey}/:nombre`,async function(req,res){
    let carta=await log.find({nombre: req.params.nombre})
 res.json(carta)
